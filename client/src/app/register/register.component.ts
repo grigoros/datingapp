951a920988f5800396c63c7dade210979c6003ac
Justin Mills <justinamills@gmail.com>
2022-08-23 15:30:24 -0400

    new components and error handling

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 
@@ -11,15 +12,15 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   register() {
     this.accountService.register(this.model).subscribe({
-      next: response => (console.log(response), this.cancel()),
-      error: error => console.log(error),
+      next: response => { console.log(response), this.cancel() },
+      error: error => { console.log(error), this.toastr.error(error.error) }
     });
   }
 
