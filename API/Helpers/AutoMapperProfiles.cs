68e8f202ba8bd53d2d6fc209b8122c9178aaa563
Justin Mills <justinamills@gmail.com>
2022-08-31 17:22:46 -0400

    adding more photo & register functionality

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,7 @@ namespace API.Helpers
                 .CalculateAge()));
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
+            CreateMap<RegisterDto, AppUser>();
         }
     }
 }
