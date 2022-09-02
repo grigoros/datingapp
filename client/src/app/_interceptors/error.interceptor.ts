663186dc12a399e79a2b1116d51e48cdf338318c
Justin Mills <justinamills@gmail.com>
2022-09-02 17:33:05 -0400

    Added like/list functionality

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -32,7 +32,7 @@ export class ErrorInterceptor implements HttpInterceptor {
               } else if (typeof(error.error) === 'object') {
                 this.toastr.error(error.statusText, error.status);
               } else {
-                this.toastr.error(error.statusText, error.status);
+                this.toastr.error(error.error, error.status);
               }
               break;
             case 401:
