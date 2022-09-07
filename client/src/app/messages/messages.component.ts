e2199f471939093a336c3439af415bf8f0e56806
Justin Mills <justinamills@gmail.com>
2022-09-07 02:57:19 -0400

    added more messaging functionality

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -9,11 +9,12 @@ import { MessageService } from '../_services/message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-  messages: Message[];
+  messages: Message[] = [];
   pagination: Pagination;
   container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService: MessageService) { }
 
@@ -22,14 +23,22 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
       next: response => {
         this.messages = response.result,
-        this.pagination = response.pagination
+        this.pagination = response.pagination,
+        this.loading = false
       }
     });
   }
   
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe({
+      next: () => this.messages.splice(this.messages.findIndex(m => m.id === id), 1)
+    });
+  }
+
   pageChanged(event: any) {
     if (this.pageNumber !== event.page) {
       this.pageNumber = event.page;
